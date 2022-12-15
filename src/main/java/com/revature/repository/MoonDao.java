108e5b434a214b7f3d7f7bb69808ad956d4d83f0
Ryan <ryan819@revature.net>
2022-12-15 14:41:17 -0500

    Completed Dao Implementation

--- src/main/java/com/revature/repository/MoonDao.java
+++ src/main/java/com/revature/repository/MoonDao.java
@@ -1,37 +1,151 @@
 package com.revature.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.revature.models.Moon;
+import com.revature.utilities.ConnectionUtil;
 
 public class MoonDao {
     
-    public List<Moon> getAllMoons() {
-		// TODO Auto-generated method stub
-		return null;
+    public List<Moon> getAllMoons() throws SQLException{
+		try (Connection connection = ConnectionUtil.createConnection()){
+			// Step 1: Create query as string
+			String sql = "select * from moons";
+			Statement statement = connection.createStatement();
+			// Step 3: Execute the query
+			ResultSet rs = statement.executeQuery(sql);
+			// Step 4: Handle the responses
+			List<Moon> moons = new ArrayList<>();
+			while(rs.next()){
+				Moon moon = new Moon();
+				moon.setId(rs.getInt(1));
+				moon.setName(rs.getString(2));
+				moon.setMyPlanetId(rs.getInt(3));
+				moons.add(moon);
+			}
+			return moons;
+		}
 	}
 
 	public Moon getMoonByName(String username, String moonName) {
-		// TODO Auto-generated method stub
-		return null;
+		try (Connection connection = ConnectionUtil.createConnection()) {
+			// NOTE TO SELF: may need to do join to username
+			
+			// Step 1: Create query as string
+			String sql = "select * from moons where name = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+			// Step 2: Provide relvant info
+            ps.setString(1, moonName);
+			
+			
+			// Step 3: Execute the query
+            ResultSet rs = ps.executeQuery();
+			// Step 4: Handle the responses
+            rs.next();
+            Moon moon = new Moon();
+            moon.setId(rs.getInt(1));
+			moon.setName(rs.getString(2));
+			moon.setMyPlanetId(rs.getInt(3));
+            return moon;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+            return new Moon();
+		}
 	}
 
 	public Moon getMoonById(String username, int moonId) {
-		// TODO Auto-generated method stub
-		return null;
+		try (Connection connection = ConnectionUtil.createConnection()) {
+			// NOTE TO SELF: may need to do join to username
+			
+			// Step 1: Create query as string
+			String sql = "select * from moons where id = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+			// Step 2: Provide relvant info
+            ps.setInt(1, moonId);
+			
+			
+			// Step 3: Execute the query
+            ResultSet rs = ps.executeQuery();
+			// Step 4: Handle the responses
+            rs.next();
+            Moon moon = new Moon();
+            moon.setId(rs.getInt(1));
+			moon.setName(rs.getString(2));
+			moon.setMyPlanetId(rs.getInt(3));
+            return moon;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+            return new Moon();
+		}
 	}
 
 	public Moon createMoon(String username, Moon m) {
-		// TODO Auto-generated method stub
-		return null;
+		try (Connection connection = ConnectionUtil.createConnection()) {
+			// NOTE TO SELF: figure out use/point of username string
+			
+			// Step 1: Create query as string
+			String sql ="insert into moons values(?, ?, ?)";
+			PreparedStatement ps = connection.prepareStatement(sql);
+			// Step 2: Provide relvant info
+			ps.setInt(1, m.getId());
+			ps.setString(2, m.getName());
+			ps.setInt(3, m.getMyPlanetId());
+			// Step 3: Execute the query
+			ps.execute();
+			// Step 4: Handle the responses
+			Moon newMoon = new Moon();
+			newMoon.setId(m.getId());
+			newMoon.setName(m.getName());
+			newMoon.setMyPlanetId(m.getMyPlanetId());
+			return newMoon;
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+            return new Moon();
+		}
 	}
 
 	public void deleteMoonById(int moonId) {
-		// TODO Auto-generated method stub
+		try (Connection connection = ConnectionUtil.createConnection()) {
+			// Step 1: Create query as string
+			String sql = "delete from moons where id = ?";
+			PreparedStatement ps = connection.prepareStatement(sql);
+			// Step 2: Provide relvant info
+			ps.setInt(1, moonId);
+			// Step 3: Execute the query
+			int rowsAffected = ps.executeUpdate();
+			// Step 4: Handle the responses
+			System.out.println("Rows Affected: " + rowsAffected);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage()); //good spot to add some logging
+		}
 	}
 
-	public List<Moon> getMoonsFromPlanet(int planetId) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Moon> getMoonsFromPlanet(int planetId) throws SQLException{
+		try (Connection connection = ConnectionUtil.createConnection()){
+			// Step 1: Create query as string
+			String sql = "select * from moons where myplanetid = ?";
+			PreparedStatement ps = connection.prepareStatement(sql);
+			// Step 2: Provide relvant info
+			ps.setInt(1, planetId);
+			// Step 3: Execute the query
+			ResultSet rs = ps.executeQuery();
+			// Step 4: Handle the responses
+			List<Moon> moons = new ArrayList<>();
+			while(rs.next()){
+				Moon moon = new Moon();
+				moon.setId(rs.getInt(1));
+				moon.setName(rs.getString(2));
+				moon.setMyPlanetId(rs.getInt(3));
+				moons.add(moon);
+			}
+			return moons;
+		}
 	}
 }
--- src/main/java/com/revature/repository/PlanetDao.java
+++ src/main/java/com/revature/repository/PlanetDao.java
@@ -1,32 +1,130 @@
 package com.revature.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.revature.models.Planet;
+import com.revature.utilities.ConnectionUtil;
 
 public class PlanetDao {
     
-    public List<Planet> getAllPlanets() {
-		// TODO Auto-generated method stub
-		return null;
+    public List<Planet> getAllPlanets() throws SQLException {
+		try (Connection connection = ConnectionUtil.createConnection()) {
+			// Step 1: Create query as string
+			String sql = "select * from planets";
+			Statement statement = connection.createStatement();
+			// Step 3: Execute the query
+			ResultSet rs = statement.executeQuery(sql);
+			// Step 4: Handle the responses
+			List<Planet> planets = new ArrayList<>();
+			while(rs.next()){
+				Planet planet = new Planet();
+				planet.setId(rs.getInt(1));
+				planet.setName(rs.getString(2));
+				planet.setOwnerId(rs.getInt(3));
+				planets.add(planet);
+			}
+			return planets;
+		}
 	}
 
 	public Planet getPlanetByName(String owner, String planetName) {
-		// TODO Auto-generated method stub
-		return null;
+		try (Connection connection = ConnectionUtil.createConnection()) {
+			// NOTE TO SELF: may need to do join to user owner
+			
+			// Step 1: Create query as string
+			String sql = "select * from planets where name = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+			// Step 2: Provide relvant info
+            ps.setString(1, planetName);
+			
+			
+			// Step 3: Execute the query
+            ResultSet rs = ps.executeQuery();
+			// Step 4: Handle the responses
+            rs.next();
+            Planet planet = new Planet();
+            planet.setId(rs.getInt(1));
+			planet.setName(rs.getString(2));
+			planet.setOwnerId(rs.getInt(3));
+            return planet;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+            return new Planet();
+		}
 	}
 
 	public Planet getPlanetById(String username, int planetId) {
-		// TODO Auto-generated method stub
-		return null;
+		try (Connection connection = ConnectionUtil.createConnection()) {
+			// NOTE TO SELF: may need to do join to user owner
+			
+			// Step 1: Create query as string
+			String sql = "select * from planets where id = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+			// Step 2: Provide relvant info
+            ps.setInt(1, planetId);
+			
+			
+			// Step 3: Execute the query
+            ResultSet rs = ps.executeQuery();
+			// Step 4: Handle the responses
+            rs.next();
+            Planet planet = new Planet();
+            planet.setId(rs.getInt(1));
+			planet.setName(rs.getString(2));
+			planet.setOwnerId(rs.getInt(3));
+            return planet;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+            return new Planet();
+		}
 	}
 
 	public Planet createPlanet(String username, Planet p) {
-		// TODO Auto-generated method stub
-		return null;
+		try (Connection connection = ConnectionUtil.createConnection()) {
+			// NOTE TO SELF: figure out use/point of username string
+			
+			// Step 1: Create query as string
+			String sql ="insert into planets values(?, ?, ?)";
+			PreparedStatement ps = connection.prepareStatement(sql);
+			// Step 2: Provide relvant info
+			ps.setInt(1, p.getId());
+			ps.setString(2, p.getName());
+			ps.setInt(3, p.getOwnerId());
+			// Step 3: Execute the query
+			ps.execute();
+			// Step 4: Handle the responses
+			Planet newPlanet = new Planet();
+			newPlanet.setId(p.getId());
+			newPlanet.setName(p.getName());
+			newPlanet.setOwnerId(p.getOwnerId());
+			return newPlanet;
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+            return new Planet();
+		}
+
 	}
 
 	public void deletePlanetById(int planetId) {
-		// TODO Auto-generated method stub
+		try (Connection connection = ConnectionUtil.createConnection()) {
+			// Step 1: Create query as string
+			String sql = "delete from planets where id = ?";
+			PreparedStatement ps = connection.prepareStatement(sql);
+			// Step 2: Provide relvant info
+			ps.setInt(1, planetId);
+			// Step 3: Execute the query
+			int rowsAffected = ps.executeUpdate();
+			// Step 4: Handle the responses
+			System.out.println("Rows Affected: " + rowsAffected);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage()); //good spot to add some logging
+		}
 	}
 }
